378a8b59bd3380549d967157052278e578c69fae
Your Name <kalaitzidispaul@gmail.com>
2023-01-11 19:56:22 -0500

    added corse

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
